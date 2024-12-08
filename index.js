ae6b9aac64542e1eb06bd57f5bfe3bf6c82e07ce
evawanjiru <eva.wanjiru@student.moringaschool.com>
2024-12-08 11:21:48 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// writeCards function
+function writeCards(names, event) {
+    let messages = []; // Create an empty array to store messages
+    
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+      messages.push(message);
+    }
+    
+    return messages;
+  }
+  
+  // countDown function
+  function countDown(n) {
+    while (n >= 0) {
+      console.log(n); // Log the current value of n
+      n--; // Decrease n by 1
+    }
+  }
+  
